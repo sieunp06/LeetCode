fe7b7bb5378d5e369373eb8036c2c10dff8c46af
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-07-15 01:21:27 +0900

    Time: 1 ms (92.03%), Space: 44.2 MB (81.20%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] answer = new int[nums.length * 2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            answer[i + nums.length] = nums[i];
+        }
+        
+        return answer;
+    }
+}
